a4e472a9ad249cb111552b4609f9eac8b021ad76
Pedro Lubacheski <70827056+pedrolubacheski@users.noreply.github.com>
2025-02-28 21:23:14 -0300

    Update Hello.java
    Criar uma branch nova e criar um método novo que escreva "Hola que tal". Salvar e Commitar as alterações.

--- Hello.java
+++ Hello.java
@@ -2,11 +2,14 @@ public class Hello {
 
     public static void main(String[] args) {
     sayHello();
+    Holaquetal();
     }
-
 
     public static void sayHello() {
         System.out.println("Hello World");
     }
 
+    public static void Holaquetal() {
+        System.out.println("Hola que tal");
+    }
 }
