7a3bef16debee37c99f5cc6fce59d89993b3ae4a
Pedro Lubacheski <70827056+pedrolubacheski@users.noreply.github.com>
2025-02-28 21:21:42 -0300

    Update Hello.java
    Criar uma branch nova e criar um método novo que escreva "Olá Mundo". Salvar e Commitar as alterações.

--- Hello.java
+++ Hello.java
@@ -2,11 +2,14 @@ public class Hello {
 
     public static void main(String[] args) {
     sayHello();
+    OlaMundo();
     }
-
 
     public static void sayHello() {
         System.out.println("Hello World");
     }
 
+    public static void OlaMundo() {
+        System.out.println("Ola Mundo");
+    }
 }
